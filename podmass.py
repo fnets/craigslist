6e9c95defa4acc8c795e42da9371500f8eed4580
fnets <kingoftherobots@gmail.com>
2016-05-17 04:04:27 -0500

    Regex Implementation
    Want to keep working, but I need to sleep.  Regex implementation is
    still grabbing whole page, rather than just the iTunes link.  Works in
    online regex testers, so may be an issue with input string.

--- podmass.py
+++ podmass.py
@@ -1,19 +1,22 @@
 import requests
 import feedparser
+import re
 
 page_links = []
 itunes_links = []
+re.M
 
 r = feedparser.parse('http://www.avclub.com/feed/rss/?feature_types=podmass')
 
 for x in xrange(0,len(r)):
-	#page_links.append(requests.get(r.entries[x].link)) #parses feed of each link in the avclub feed
-	for y in xrange(0,len(r.entries[x].links)):
-		page_links.append(r.entries[x].links[y].href) 
-'''* for y in xrange(0,len(page_links)):
-		#itunes_links = itunes_links.append(page_links.entries[y].link) 
+	page_links.append(requests.get(r.entries[x].link)) #parses feed of each link in the avclub feed
+	
+	for y in xrange(1,len(page_links)): 
 		
-
-'''
-print page_links
+		itunes_links.append(re.search('(?<=href=").*itunes.*(?=")', page_links[y].text).string)
+		
+#regex output tends to be way too long, so I output it to the file below
+f = open('regex_out.txt', 'w')
+f.write(page_links[1].text.encode('utf8'))
+f.close()
 
