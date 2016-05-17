35aff139d0549c57707027e52f7a4a52761b2fdd
fnets <kingoftherobots@gmail.com>
2016-05-17 05:06:50 -0500

    Workable output
    Regex outputs only itunes links to text file regex_out.txt, but there
    are some that are repeated.  Worth staying up until 5 AM for!

--- podmass.py
+++ podmass.py
@@ -4,19 +4,19 @@ import re
 
 page_links = []
 itunes_links = []
-re.M
 
-r = feedparser.parse('http://www.avclub.com/feed/rss/?feature_types=podmass')
+f = open('regex_out.txt', 'w') #opens output file, because there are too many links to be readable on command line
+
+r = feedparser.parse('http://www.avclub.com/feed/rss/?feature_types=podmass') #loads podmass page
 
 for x in xrange(0,len(r)):
 	page_links.append(requests.get(r.entries[x].link)) #parses feed of each link in the avclub feed
 	
-	for y in xrange(1,len(page_links)): 
+	for y in xrange(0,len(page_links)): 
 		
-		itunes_links.append(re.search('(?<=href=").*itunes.*(?=")', page_links[y].text).string)
-		
-#regex output tends to be way too long, so I output it to the file below
-f = open('regex_out.txt', 'w')
-f.write(page_links[1].text.encode('utf8'))
-f.close()
+		itunes_links.append(re.findall('(?<=href=").*itunes.*(?=")', page_links[y].text)) #searches through currently accessed podmass pages' download links for itunes links.
+		f.write(itunes_links[x][y].encode('utf8')+"\n") #writes added link to file
+
+f.close() #closes file
+
 
