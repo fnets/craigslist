497d2d94266f232c37425d01319ad4fbdbf2163a
fnets <kingoftherobots@gmail.com>
2016-05-18 08:06:27 -0500

    Just wanted to revert
    The only reason I made this commit is to resolve conflicts

--- podmass.py
+++ podmass.py
@@ -5,11 +5,11 @@ import sys
 
 page_links = []
 itunes_links = []
+re.M
 
-f = open('regex_out.txt', 'w') #opens output file, because there are too many links to be readable on command line
+r = feedparser.parse('http://www.avclub.com/feed/rss/?feature_types=podmass')
 
-r = feedparser.parse('http://www.avclub.com/feed/rss/?feature_types=podmass') #loads podmass page
-
+<<<<<<< HEAD
 page_links.append(requests.get(r.entries[0].link))
 for y in xrange(0,len(page_links)): 
 		
@@ -42,4 +42,17 @@ print page_links
 
 f.close() #closes file
 
+=======
+for x in xrange(0,len(r)):
+	page_links.append(requests.get(r.entries[x].link)) #parses feed of each link in the avclub feed
+	
+	for y in xrange(1,len(page_links)): 
+		
+		itunes_links.append(re.search('(?<=href=").*itunes.*(?=")', page_links[y].text).string)
+		
+#regex output tends to be way too long, so I output it to the file below
+f = open('regex_out.txt', 'w')
+f.write(page_links[1].text.encode('utf8'))
+f.close()
+>>>>>>> parent of 35aff13... Workable output
 
