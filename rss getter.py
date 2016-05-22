a0e9d9df39e19bc5fa69d4f7108a7b5d178bfc0f
fnets <kingoftherobots@gmail.com>
2016-05-22 13:08:10 -0500

    Feedflipper Complete
    Input and copy from feedflipper site is complete.  Needs to write to
    file, and input into SQL.  Finally, seems to time out page after a few
    entries.  It's probably worth setting a timer that lets the page have a
    break.

--- rss getter.py
+++ rss getter.py
@@ -1,9 +1,28 @@
  #all of this works, so implement it in a loop that reads your iTunes links.  Then output to CSV, then output to SQL DB
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome()
+rss = [] #store rss links
  
- driver.get('http://www.picklemonkey.net/feedflipper-home')
- text_here = driver.find_element_by_id('feed')
- text_here.send_keys('http://itunes.apple.com/us/podcast/comedy-bang-bang-the-podcast/id316045799?mt=2')
- result = driver.find_element_by_id("extracted_podcast")
-  print result.text
-http://rss.earwolf.com/comedy-bang-bang
- driver.get(result.text)
+driver.get('http://www.picklemonkey.net/feedflipper-home') #load webtool
+
+ 
+with open('C:/Users/Frank/Desktop/''craigslist regex bk''/regex_out.txt') as f:
+	iTunesLinks = f.readlines()
+
+for link in iTunesLinks: #go through regex output of podmass.py and add use them in the webtool
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "feed")))
+	text_here = driver.find_element_by_id('feed') #find input field
+	text_here.send_keys(link)
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "extracted_podcast")))
+	result = driver.find_element_by_id("extracted_podcast") #read rss feed, timing may need to be added
+	rss.append(result) #writes output of field to list.
+	print result
+	driver.refresh() #reloads page
+
+f.close()
+driver.close()
