5ce8e136f79ec3b8c5db99a20c519e4044f952ca
fnets <kingoftherobots@gmail.com>
2016-06-27 00:52:16 -0500

    Found Why href was returning php link
    href for extracted rss link changes dynamically.  So, at first it is the
    php link, after running it is the desired link.  Further instructions on
    how to solve are in the top comment lines in rss getter.py

--- rss getter.py
+++ rss getter.py
@@ -1,4 +1,11 @@
- #all of this works, so implement it in a loop that reads your iTunes links.  Then output to CSV, then output to SQL DB
+##
+## EXTRACTED_PODACAST HREF CHANGES DYNAMICALLY
+## CHECK OLD "EXTRACTED_PODCAST" HREF AGAINST NEW ONE
+## IF FALSE, SHOULD GET THE VALUE
+##
+##
+## OR AFTER SCRIPT OR FUNCTIONS COMPLETES LABEL ID PODCAST_ERROR SHOULD HAVE ATTRIBUTE DISPLAY:NONE
+##
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,6 +14,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 rss = [] #store rss links
+driver.implicitly_wait(30)
  
 driver.get('http://www.picklemonkey.net/feedflipper-home') #load webtool
 
@@ -15,13 +23,15 @@ with open('C:/Users/Frank/Documents/GitHub/craigslist/regex_out_top.txt') as f:
 	iTunesLinks = f.readlines()
 
 for link in iTunesLinks: #go through regex output of podmass.py and add use them in the webtool
-	element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "feed")))
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "feed")))
 	text_here = driver.find_element_by_id('feed') #find input field
 	text_here.send_keys(link)
-	element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "extracted_podcast")))
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "extracted_podcast")))
 	result = driver.find_element_by_id("extracted_podcast") #read rss feed, timing may need to be added
 	rss.append(result) #writes output of field to list.
-	print result.get_attribute('href')
+	print result.text
+	print result.get_attribute('href')	
+
 	driver.refresh() #reloads page
 
 f.close()
